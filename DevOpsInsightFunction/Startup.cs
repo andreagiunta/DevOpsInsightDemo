13baad95c4c47939f2762181450a75fde1f4e235
Andrea Giunta <angiunta@microsoft.com>
2022-06-24 14:18:32 +0200

    Draft storing on azure sql

--- DevOpsInsightFunction/Startup.cs
+++ DevOpsInsightFunction/Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevOpsInsightFunction.OrmLayer;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 [assembly: FunctionsStartup(typeof(DevOpsInsightFunction.Startup))]
 namespace DevOpsInsightFunction
@@ -14,6 +16,7 @@ namespace DevOpsInsightFunction
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<IDataStore>((ds) => { return new DataStore(); });
         }
     }
 
